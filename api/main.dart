4b7a15a6861fb411bcb85047dfb4487bb11760f7
Marcus Twichel <marc.twichy@gmail.com>
2025-09-25 12:47:22 -0600

    fix analysis errors

--- api/main.dart
+++ api/main.dart
@@ -10,9 +10,5 @@ Future<void> init(InternetAddress ip, int port) async {
 }
 
 Future<HttpServer> run(Handler handler, InternetAddress ip, int port) {
-  // 1. Execute any custom code prior to starting the server...
-
-  // 2. Use the provided `handler`, `ip`, and `port` to create a custom `HttpServer`.
-  // Or use the Dart Frog serve method to do that for you.
   return serve(handler, ip, port);
 }
